31edb3e19e7712fe6f93c7344cda0e62a8175b45
shin72mican <shin72mican@gmail.com>
2025-08-16 15:49:52 +0900

    バックエンドアプリ

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public Map<String, String> hello() {
+		Map<String, String> test = new HashMap<>();
+		test.put("Hello1", "こんにちは");
+		test.put("Hello2", "こんにちは");
+		test.put("Hello3", "こんにちは");
+		return test;
+	}
+}
